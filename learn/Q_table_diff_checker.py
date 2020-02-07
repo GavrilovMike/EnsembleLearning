614a168ed0f0e285140d242fe2a84e0b935dbe4d
Mikhail Gavrilov <mgavrilov@aquivalabs.com>
2020-02-07 08:59:49 +0300

    Diff checker fixes

--- learn/Q_table_diff_checker.py
+++ learn/Q_table_diff_checker.py
@@ -1,11 +1,11 @@
 import pickle
 
 
-with open("/Users/mgavrilov/Study/ENSEMBLEALGS/ensebmles/Bagging/Bagging_QTable.pkl", 'rb') as f:
+with open("/Users/mgavrilov/Study/ENSEMBLEALGS/ensebmles/Voting/Voting_QTable.pkl", 'rb') as f:
     Q_table = pickle.load(f)
     print('Q_table first: \n',Q_table)
 
-with open("/Users/mgavrilov/Study/ENSEMBLEALGS/ensebmles/Stacking/Ada_Boost_QTable.pkl", 'rb') as f2:
+with open("/Users/mgavrilov/Study/ENSEMBLEALGS/ensebmles/Bagging/Bagging_QTable.pkl", 'rb') as f2:
     Q_table_2 = pickle.load(f2)
     print('Q_table second: \n', Q_table_2)
 
